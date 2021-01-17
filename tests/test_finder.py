1341a38c109a156afa6574a503f9ee46648261d4
Paul Moore <p.f.moore@gmail.com>
2021-01-17 16:36:05 +0000

    Add some more tests

--- tests/test_finder.py
+++ tests/test_finder.py
@@ -12,7 +12,7 @@ FILES = [
     "proj-0.1-py2.py3-none-any.whl",
 ]
 
-class DummyCandidate(Candidate):
+class TestCandidate(Candidate):
     def __init__(self, filename):
         self.filename = filename
         self.attributes_from_filename(filename)
@@ -20,18 +20,14 @@ class DummyCandidate(Candidate):
         self.is_yanked = False
 
 
-def dummy_source(name):
-    for filename in FILES:
-        c = DummyCandidate(filename)
-        if c.name == name:
-            yield c
-
-
-def test_finder():
+def test_finder_ordering():
+    def src(name):
+        for filename in FILES:
+            c = TestCandidate(filename)
+            if c.name == name:
+                yield c
     f = Finder(
-        sources = [
-            dummy_source,
-        ],
+        sources = [src],
         compatibility_tags = [Tag("py3", "none", "any")],
         python_version = Version("3.8"),
     )
@@ -42,4 +38,66 @@ def test_finder():
         "proj-0.2.tar.gz",
         "proj-0.1-py2.py3-none-any.whl",
         "proj-0.1.tar.gz",
+    ]
+
+def test_finder_matches_tags():
+    def src(name):
+        for filename in FILES:
+            c = TestCandidate(filename)
+            if c.name == name:
+                yield c
+    f = Finder(
+        sources = [src],
+        compatibility_tags = [Tag("py2", "none", "any")],
+        python_version = Version("2.7"),
+    )
+
+    assert [c.filename for c in f.get_candidates(Requirement("proj"))] == [
+        "proj-0.3.tar.gz",
+        "proj-0.2.tar.gz",
+        "proj-0.1-py2.py3-none-any.whl",
+        "proj-0.1.tar.gz",
+    ]
+
+def test_finder_respects_requires_python():
+    def src(name):
+        for filename in FILES:
+            c = TestCandidate(filename)
+            if '0.1' in filename:
+                c.requires_python = SpecifierSet("<=3.7")
+            if c.name == name:
+                yield c
+
+    f = Finder(
+        sources = [src],
+        compatibility_tags = [Tag("py3", "none", "any")],
+        python_version = Version("3.8"),
+    )
+
+    assert [c.filename for c in f.get_candidates(Requirement("proj"))] == [
+        "proj-0.3.tar.gz",
+        "proj-0.2-py3-none-any.whl",
+        "proj-0.2.tar.gz",
+    ]
+
+def test_finder_respects_yanked():
+    def src(name):
+        for filename in FILES:
+            c = TestCandidate(filename)
+            if '0.1' in filename:
+                c.is_yanked = True
+            if c.name == name:
+                yield c
+
+    f = Finder(
+        sources = [src],
+        compatibility_tags = [Tag("py3", "none", "any")],
+        python_version = Version("3.8"),
+        allow_yanked=False,
+    )
+
+    assert [c.filename for c in f.get_candidates(Requirement("proj"))] == [
+        "proj-0.3.tar.gz",
+        "proj-0.2-py3-none-any.whl",
+        "proj-0.2.tar.gz",
     ]
